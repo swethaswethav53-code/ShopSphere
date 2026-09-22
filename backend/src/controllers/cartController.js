4d72260c4df4d40dc5ce5aed6f7373ab704c367f
swethaswethav53-code <swethaswethav53@gmail.com>
2026-09-22 19:06:33 +0530

    fix path

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -1,5 +1,5 @@
 const { logActivity } = require('../middleware/activityLogger');
-const Cart = require('../models/cart');
+const Cart = require('../models/Cart');
 const Product = require('../models/product');
 
 // @desc    Get logged-in user's cart
